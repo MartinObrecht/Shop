734686ef6a29d3aad6a4ad1544356a79ab4d30fa
Martin Obrecht <martin.obrecht@yahoo.com.br>
2021-09-21 15:57:52 -0300

    Controllers criados

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Get()
+        {
+            return new List<Category>();
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> GetById(int id)
+        {
+            return new Category();
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Post([FromBody]Category model)
+        {
+            return Ok(model);
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<ActionResult<List<Category>>> Put(int id, Category model)
+        {
+            if(model.Id == id)
+                return Ok(model);
+            
+            return null;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult<List<Category>>> Delete()
+        {
+            return Ok();
+        }
+    }
+}
